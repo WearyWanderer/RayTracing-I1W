8116c81493b771ab2e69d70013181f07274a486d
Andrew Ayre <andrewayregames@live.co.uk>
2018-04-22 20:40:12 +0100

    Added ray class
    TODO: setup up class maybe for camera or possibly just follow tutorial still for now

--- PathRayTracer/PathRayTracer/PathRayTracer.cpp
+++ PathRayTracer/PathRayTracer/PathRayTracer.cpp
@@ -34,7 +34,19 @@ void WriteToBaseImageFilePPM(int width, int height, const char* filename)
 
 bool WriteBMPFile(const int w,const int h, int comp, const void* data)
 {	
-	unsigned char* colDataRaw = new unsigned char[(w*h)*3];
+	if (stbi_write_bmp("example.bmp", w, h, 3, data) == 0)
+		return false;
+
+	return true;
+}
+
+
+
+int main()
+{
+	int w = 200, h = 100;
+
+	unsigned char* colDataRaw = new unsigned char[(w*h) * 3];
 	int pix = 0;
 	for (int j = h - 1; j >= 0; j--)
 	{
@@ -47,24 +59,14 @@ bool WriteBMPFile(const int w,const int h, int comp, const void* data)
 			int ib = int(255.99f*col[2]);
 
 			colDataRaw[pix] = ir;
-			colDataRaw[pix+1] = ig;
-			colDataRaw[pix+2] = ib;
-			pix+=3;
+			colDataRaw[pix + 1] = ig;
+			colDataRaw[pix + 2] = ib;
+			pix += 3;
 		}
 	}
 
-	if (stbi_write_bmp("example.bmp", w, h, 3, &colDataRaw[0]) == 0)
-		return false;
-
-	return true;
-}
-
-int main()
-{
-	int xSize = 200, ySize = 100;
-
 	//WriteToBaseImageFilePPM(xSize, ySize, "example.ppm");
-	WriteBMPFile(xSize, ySize, 3, nullptr);
+	WriteBMPFile(w, h, 3, &colDataRaw[0]);
 
 	cout << "Press Enter key to close" << endl;
 	cin.get();
