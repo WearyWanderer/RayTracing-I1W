b67d8b5b8d5bce91d01555d1d4aa0410eb86d22a
Andrew Ayre <andrewayregames@live.co.uk>
2018-05-01 12:35:15 +0100

    Simple diffuse test
    Eliminated shadow acne, but could use a nicer Distribution factory, used by all for generating static random numbers. Prevents overhead of generating lots of random generators (this crashed the app originally)

--- PathRayTracer/PathRayTracer/Maths/ray.h
+++ PathRayTracer/PathRayTracer/Maths/ray.h
@@ -1,6 +1,9 @@
 #pragma once
 #ifndef RAYH
 #define RAYH
+
+#include <random>
+
 #include "vec3.h"
 
 class ray
@@ -15,6 +18,16 @@ public:
 
 	vec3 A;
 	vec3 B;
+
+	static vec3 random_in_unit_sphere(std::default_random_engine *engine, std::uniform_real_distribution<float> randomFloat)
+	{
+		vec3 p;
+		do 
+		{
+			p = 2.0f * vec3(randomFloat(*engine), randomFloat(*engine), randomFloat(*engine)) - vec3(1.0f, 1.0f, 1.0f);
+		} while (p.squared_length() >= 1.0f);
+		return p;
+	}
 };
 
 #endif
--- PathRayTracer/PathRayTracer/PathRayTracer.cpp
+++ PathRayTracer/PathRayTracer/PathRayTracer.cpp
@@ -14,15 +14,31 @@
 
 using namespace std;
 
-bool WriteBMPFile(const int w,const int h, int comp, const void* data)
+bool WriteBMPFile(const char* filename, const int w,const int h, int comp, const void* data)
 {	
-	if (stbi_write_bmp("example.bmp", w, h, 3, data) == 0)
+	if (stbi_write_bmp(filename, w, h, 3, data) == 0)
 		return false;
 
 	return true;
 }
 
-vec3 color(const ray& r, hitable *world)
+vec3 color(const ray& r, hitable *world, default_random_engine *engine, uniform_real_distribution<float> *randomFloat)
+{
+	hit_record rec;
+	if (world->hit(r, 0.001f, FLT_MAX, rec))
+	{
+		vec3 target = rec.p + rec.normal + ray::random_in_unit_sphere(engine, *randomFloat); //Get a random ray bounce target
+		return 0.5f * color(ray(rec.p, target-rec.p), world, engine, randomFloat); //Normalise to 0 to 1 range, from -1 to -1
+	}
+	else
+	{
+		vec3 unit_direction = unit_vector(r.direction());
+		float t = 0.5f * (unit_direction.y() + 1.0f);
+		return (1.0f - t)*vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+	}
+}
+
+vec3 colorNormals(const ray& r, hitable *world)
 {
 	hit_record rec;
 	if (world->hit(r, 0.0f, FLT_MAX, rec))
@@ -39,18 +55,19 @@ vec3 color(const ray& r, hitable *world)
 
 int main()
 {
-	int w = 800, h = 400, samplesPerPixel = 100;
+	int w = 1600, h = 800, samplesPerPixel = 100;
 
 #pragma region Camera Setup
 	camera cam;
 #pragma endregion
 
 #pragma region World Setup
-	hitable *list[2]; //create array for our hitable_list
+	hitable *list[3]; //create array for our hitable_list
 	list[0] = new sphere(vec3(0.0f, 0.0f, -1.0f), 0.5f);
 	list[1] = new sphere(vec3(0.0f, -100.5f, -1.0f), 100.0f);
+	list[2] = new sphere(vec3(1.0f, 0.0f, -1.5f), 0.5f);
 
-	hitable *world = new hitable_list(list, 2); //create our list which fully represents our 'world' of hitable objects
+	hitable *world = new hitable_list(list, 3); //create our list which fully represents our 'world' of hitable objects
 #pragma endregion
 
 #pragma region Random Setup
@@ -69,10 +86,10 @@ int main()
 			{
 				float u = (float)(i + randomFloat(randEngine)) / (float)w, v = (float)(j + randomFloat(randEngine)) / (float)h;
 				ray r = cam.get_ray(u, v);
-				pixelCol += color(r, world);
+				pixelCol += color(r, world, &randEngine, &randomFloat);
 			}
 			pixelCol /= samplesPerPixel;
-
+			pixelCol = vec3(sqrt(pixelCol[0]), sqrt(pixelCol[1]), sqrt(pixelCol[2]));
 			int ir = int(255.99f*pixelCol[0]);
 			int ig = int(255.99f*pixelCol[1]);
 			int ib = int(255.99f*pixelCol[2]);
@@ -84,7 +101,7 @@ int main()
 		}
 	}
 
-	WriteBMPFile(w, h, 3, &colDataRaw[0]);
+	WriteBMPFile("earlydiffuse.bmp", w, h, 3, &colDataRaw[0]);
 
 	cout << "Press Enter key to close" << endl;
 	cin.get();
